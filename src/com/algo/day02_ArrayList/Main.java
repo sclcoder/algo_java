0dae2686fd4b74064ee104a5b37a097e804c9eff
scl <sclcoder@163.com>
2019-04-15 21:52:16 +0800

    01-复杂度

---
+++ src/com/algo/day02_ArrayList/Main.java
@@ -0,0 +1,12 @@
+package com.algo.day02_ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int array[] = new int[]{10, 11, 12, 13};
+
+        ArrayList list  = new ArrayList();
+        list.add(22);
+        list.add(0, 33);
+    }
+}
