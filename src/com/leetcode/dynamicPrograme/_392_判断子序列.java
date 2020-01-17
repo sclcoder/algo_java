38f8655bfc2543d1b6c603a0f37ba71523638994
scl <sclcoder@163.com>
2020-01-17 18:13:29 +0800

    判断子序列dp

--- src/com/leetcode/dynamicPrograme/_392_判断子序列.java
+++ src/com/leetcode/dynamicPrograme/_392_判断子序列.java
@@ -2,7 +2,7 @@ package com.leetcode.dynamicPrograme;
 
 public class _392_判断子序列 {
     public static void main(String[] args) {
-        System.out.println(isSubsequence("abc","cahbgdxc"));
+        System.out.println(isSubsequence2("abc","cahbgdxc"));
     }
 
     // 双指针
@@ -18,18 +18,28 @@ public class _392_判断子序列 {
         return i == s.length();
     }
 
-    // 动态规划
+    /**  动态规划 状态转移通过dp-table来思考
+     *        a  c  b  d  c  f  d
+     *    a   1  1  1  1  1  1  1
+     *    b   0  0  1  1  1  1  1
+     *    c   0  0  0  0  1  1  1
+     */
     public static boolean isSubsequence2 (String s, String t){
         int slen = s.length();
         int tlen = t.length();
         if (slen > tlen) return false;
         if (slen == 0) return true;
         boolean[][] dp = new boolean[slen][tlen];
-        for (int j = 0; j < slen; j++) {
-            dp[0][j] = false;
-        }
         for (int i = 0; i < slen; i++) {
-            for (int j = 0; j < tlen; j++) {
+            dp[i][0] = false;
+        }
+        for (int i = 0; i < tlen; i++) {
+            dp[0][i] = true;
+        }
+        dp[0][0] = s.charAt(0) == t.charAt(0);
+
+        for (int i = 1; i < slen; i++) {
+            for (int j = 1; j < tlen; j++) {
                 if (s.charAt(i) == t.charAt(j)){
                     dp[i][j] =  dp[i-1][j-1];
                 } else {
@@ -37,7 +47,7 @@ public class _392_判断子序列 {
                 }
             }
         }
-        return true;
+        return dp[slen-1][tlen-1];
     }
 
     /**
